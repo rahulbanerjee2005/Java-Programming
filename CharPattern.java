8c9c7b394a9434e96b2220b1517f499105472490
Rahul Banerjee <rahul2020banerjee@gmail.com>
2025-08-31 21:45:05 +0530

    JAVA PATTERNS

---
+++ CharPattern.java
@@ -0,0 +1,14 @@
+public class CharPattern {
+    public static void main(String[] args) {
+        int n=4;
+        char ch= 'A';
+        for(int i=1; i<=n; i++){    //i= line
+            for(int j=1; j<=i; j++){    //j= characters
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+    
+}
